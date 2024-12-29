e8cbc43db54ade00fd17fec336fce0f379034b0d
Karan Kamble <karanbk9375@gmail.com>
2024-12-29 22:32:46 +0530

    test

--- HIBERNATEV7/src/test/java/test/Testing.java
+++ HIBERNATEV7/src/test/java/test/Testing.java
@@ -11,40 +11,40 @@ public class Testing {
 	public void open_account() {
 
 		AccountService service = new AccountServiceImpl();
-		
+
 		Account account = new Account();
 		account.setName("Raghav");
 		account.setType("Saving");
 		account.setBalance(8000);
-		
+
 		service.openAccount(account);
 	}
-	
+
 	@Test
 	public void withdraw_amount() {
 		AccountService service = new AccountServiceImpl();
 		service.withdraw(5, 1000);
-		
+
 	}
-	
+
 	@Test
 	public void deposit_amount() {
 		AccountService service = new AccountServiceImpl();
 		service.deposit(4, 200);
-		
+
 	}
-	
+
 	@Test
 	public void transfer_amount() {
 		AccountService service = new AccountServiceImpl();
-		service.transfer(1,4,1000);
-		
+		service.transfer(1, 4, 1000);
+
 	}
-	
+
 	@Test
 	public void mini_statement() {
 		AccountService service = new AccountServiceImpl();
 		service.miniStatament(4);
-		
+
 	}
 }
